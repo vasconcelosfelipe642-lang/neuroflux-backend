4a01a873e467e42ac57f476daa0d34b7b54bd980
Jorge Felipe <jorge.ft.mendonca@gmail.com>
2026-05-13 12:08:29 -0300

    Ajustes no Banco de Dados

---
+++ config/database.js
@@ -0,0 +1,19 @@
+module.exports = {
+  dialect: 'mysql',
+
+  host: process.env.DB_HOST,
+
+  port: process.env.DB_PORT,
+
+  username: process.env.DB_USER,
+
+  password: process.env.DB_PASSWORD,
+
+  database: process.env.DB_NAME,
+
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
