d7d8ded26d69b3409bb48cafdfcb9e162990f22f
vasconcelosfelipe642-lang <vasconcelosfelipe642@gmail.com>
2026-05-13 01:11:33 -0300

    mudança no server pra organizar o mysql

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const db = require('./models');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+// Sincroniza o banco e inicia o servidor
+db.sequelize.authenticate()
+  .then(() => {
+    console.log(' Conectado ao MySQL com sucesso!');
+    app.listen(PORT, () => console.log(`Servidor Neuroflux em http://localhost:${PORT}`));
+  })
+  .catch(err => console.error(' Erro de conexão:', err));
