0a7b09fc31ae10f4162306a87595aab9d3eabadc
yuri cruz <yuricruz063@gmail.com>
2026-05-13 16:32:39 -0300

    criação e configuração do middleware auth

---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; 
+
+  if (token == null) {
+    return res.status(401).json({ message: 'Não autorizado' }); 
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token inválido ou expirado' }); 
+    }
+
+    req.user = user; 
+    next(); 
+  });
+};
