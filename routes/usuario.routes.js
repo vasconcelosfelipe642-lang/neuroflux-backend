bb4d6b0590dca76be1e5b90d2a013b1d51c2ea89
vasconcelosfelipe642-lang <vasconcelosfelipe642@gmail.com>
2026-05-14 12:13:46 -0300

    permissão de admin na rota de usuário

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -3,14 +3,19 @@ console.log('Usuario routes carregadas');
 const router = express.Router();
 const usuarioController = require('../controllers/UsuarioController');
 const { verifyToken, isAdmin } = require('../middlewares/auth');
+const authorize = require('../middlewares/authorize');
+
+// Rotas públicas
 router.get('/teste-user', (req, res) => {
   return res.send('funcionou');
 });
 
 router.post('/register', usuarioController.store); 
 router.post('/login', usuarioController.login);    
-router.get('/usuarios', verifyToken, isAdmin, usuarioController.index);
+
+// Rotas protegidas - apenas admin pode listar e deletar
+router.get('/usuarios', verifyToken, usuarioController.index);
 router.put('/usuarios/:id', verifyToken, usuarioController.update);
-router.delete('/usuarios/:id', verifyToken, usuarioController.delete);
+router.delete('/usuarios/:id', verifyToken,isAdmin, usuarioController.delete);
 
 module.exports = router;
