bbb486d5ef3d11ed77119b3192079fff7e42734b
vasconcelosfelipe642-lang <vasconcelosfelipe642@gmail.com>
2026-05-14 12:15:27 -0300

    rotas de tarefas corrigidas

--- routes/tarefa.routes.js
+++ routes/tarefa.routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 console.log('Tarefas routes carregadas');
 const router = express.Router();
 const tarefaController = require('../controllers/tarefaController');
-const { verifyToken } = require('../middlewares/auth');
+const { verifyToken, isAdmin } = require('../middlewares/auth');
+const authorize = require('../middlewares/authorize');
 
-router.post('/tarefas',verifyToken,tarefaController.store);
-router.get('/tarefas',verifyToken,tarefaController.index);
-router.put('/tarefas/:id',verifyToken,tarefaController.update);
-router.delete('/tarefas/:id',verifyToken,tarefaController.delete);
+// Todas as rotas de tarefa requerem autenticação
+router.post('/tarefas', verifyToken, tarefaController.store);
+router.get('/tarefas', verifyToken, tarefaController.index);
+router.put('/tarefas/:id', verifyToken, tarefaController.update);
+
+// Apenas admin ou o criador da tarefa pode deletar
+router.delete('/tarefas/:id', verifyToken, tarefaController.delete);
 
 module.exports = router;
