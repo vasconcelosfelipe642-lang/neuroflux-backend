7ac7d760bbb45ecc2cc2e7825550a5530a164850
vasconcelosfelipe642-lang <vasconcelosfelipe642@gmail.com>
2026-08-04 16:54:48 -0300

    fix: adiciona suporte a ssl para o mysql

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
 module.exports = {
   dialect: 'mysql',
-
   host: process.env.DB_HOST,
-
   port: process.env.DB_PORT,
-
   username: process.env.DB_USER,
-
   password: process.env.DB_PASSWORD,
-
   database: process.env.DB_NAME,
-
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   define: {
     timestamps: true,
     underscored: true,
